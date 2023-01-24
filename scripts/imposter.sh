e5fc2b552ff8111a2de5c1b3e29c4ded4a3f9fae
AF <4775679+datalek@users.noreply.github.com>
2023-01-24 11:19:00 +0100

    General fixes (#10)
    * Fix mockoon
    * Fix imposter
    * Align table
    * Update specmatic

--- scripts/imposter.sh
+++ scripts/imposter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -ti --rm -p 8080:8080 -v "$PWD/docs:/opt/imposter/config" outofcoffee/imposter-openapi
+docker run -ti --rm -p 8080:8080 -v "$PWD/docs/openapi:/opt/imposter/config/openapi" -v "$PWD/docs/imposter:/opt/imposter/config" outofcoffee/imposter-openapi
